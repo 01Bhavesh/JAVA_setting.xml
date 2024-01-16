d8e0f55f0b006d060f8f25af9b9e3e17264d5b9d
Bhavesh Gharat <144714226+01Bhavesh@users.noreply.github.com>
2024-01-16 09:38:47 +0530

    Add files via upload

---
+++ SingleTable/src/main/java/com/demo/test/TestGetProduct.java
@@ -0,0 +1,26 @@
+package com.demo.test;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import com.demo.model.Perishable;
+
+
+
+public class TestGetProduct {
+
+	public static void main(String[] args) {
+		SessionFactory sf=new Configuration().configure().buildSessionFactory();
+		Session session=sf.openSession();
+		Transaction tr=session.beginTransaction();
+		Perishable p=session.get(Perishable.class, 12);
+		tr.commit();
+		session.close();
+		sf.close();
+		System.out.println(p);
+
+	}
+
+}
